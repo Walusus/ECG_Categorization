fc4c53f83d149387ccbd3d1d46b395d3188d829b
Walusus <marcinw.99.mw@gmail.com>
2020-05-21 19:07:06 +0200

    start working on skeleton

---
+++ cnn_attempt.py
@@ -0,0 +1,8 @@
+# TODO Load mit-bih training dataset using pandas and convert to numpy.
+pass
+
+# TODO Separate indexes of each class.
+pass
+
+# TODO Augment missing data (experiment with different values).
+pass
