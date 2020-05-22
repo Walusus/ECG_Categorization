502de77950b55d792b8303a8d73c6baf41c1068b
Walusus <marcinw.99.mw@gmail.com>
2020-05-22 18:59:31 +0200

    add net model structure

---
+++ cnn_net_model.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+
+
+class CnnNet(nn.Module):
+
+    def __init__(self):
+        super(CnnNet, self).__init__()
+        self.input_conv = nn.Conv1d(1, 32, 5, padding=2)
+        self.rec_conv = nn.Conv1d(32, 32, 5, padding=2)
+        self.activation = nn.ReLU()
+        self.pool = nn.MaxPool1d(5, stride=2)
+        self.fc1 = nn.Linear(256, 32)
+        self.fc2 = nn.Linear(32, 5)
+
+    def forward(self, x):
+        prev_x = self.input_conv(x)
+
+        for i in range(5):
+            x = self.rec_conv(prev_x)
+            x = self.activation(x)
+            x = self.rec_conv(x) + prev_x
+            x = self.activation(x)
+            prev_x = self.pool(x)
+
+        prev_x = torch.flatten(prev_x)
+        x = self.fc1(x)
+        x = self.activation(x)
+        x = self.fc2(x)
+        x = nn.Softmax()(x)
+
+        return x
