fc021f128c1df11eee34e976548d38365ad453a3
Walusus <marcinw.99.mw@gmail.com>
2020-06-05 20:36:31 +0200

    cnn model and training

---
+++ mnist/cnn_attempt.py
@@ -0,0 +1,123 @@
+import torch
+import torchvision
+import matplotlib.pyplot as plt
+import torchvision.transforms as transforms
+import sklearn.metrics as mtr
+import numpy as np
+import seaborn as sns
+from mnist.cnn_net_model import CnnNet
+import gc
+
+
+# Check for cuda presence
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# Import the dataset
+transform = transforms.ToTensor()
+train_set = torchvision.datasets.MNIST(
+    "dataset/", train=True, download=False,
+    transform=transforms.Lambda(lambda x: transform(x).to(device=device)),
+    target_transform=transforms.Lambda(lambda x: torch.tensor(x, device=device))
+)
+test_set = torchvision.datasets.MNIST(
+    "dataset/", train=False, download=False,
+    transform=transforms.Lambda(lambda x: transform(x).to(device=device)),
+    target_transform=transforms.Lambda(lambda x: torch.tensor(x, device=device))
+)
+
+# Present few data samples
+plt.figure()
+for i in range(4):
+    x, y = test_set[i]
+    plt.subplot(221 + i)
+    plt.imshow(transforms.ToPILImage()(x.cpu()), cmap='gray')
+    plt.title(f"Label: {y:d}")
+plt.show()
+
+# Initialize loaders
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=150, shuffle=True)
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=250, shuffle=True)
+
+# Import network motel
+net = CnnNet().to(device=device)
+
+# Choose loss criterion, optimiser and learning rate
+optim = torch.optim.Adam(net.parameters(), lr=1e-5)
+criterion = torch.nn.CrossEntropyLoss()
+
+
+test_loss_track = []
+train_loss_track = []
+epochs_num = 5
+for epoch in range(epochs_num):
+    # Train network
+    net.train()
+    for batch_num, (inputs, labels) in enumerate(train_loader):
+        optim.zero_grad()
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optim.step()
+        train_loss_track.append(loss.item())
+
+        # Printing log each 2nd batch
+        if batch_num % 5 == 0:
+            print(f"Epoch: {epoch:d}/{epochs_num:d} ({epoch/epochs_num*100:.1f}%),"
+                  f"\tMini-batch: {batch_num:d},\tLoss: {loss.item():f}")
+        # Cleanup
+        del inputs, outputs, labels, loss
+        gc.collect()
+
+    # Test network every epoch
+    net.eval()
+    loss_sum = 0
+    accuracy_sum = 0
+    num = 0
+    for batch_num, (inputs, labels) in enumerate(test_loader):
+        outputs = net(inputs)
+        _, pred_labels = outputs.cpu().detach().max(1)
+        loss_sum += criterion(outputs, labels).item()
+        accuracy_sum += mtr.accuracy_score(labels.cpu(), pred_labels)
+        num += 1
+        # Cleanup
+        del inputs, outputs, labels, pred_labels, _
+        gc.collect()
+
+    accuracy = accuracy_sum / num
+    loss = loss_sum / num
+    test_loss_track.append(loss)
+    print(f"Test accuracy: {100*accuracy:.1f}%,\tTest loss: {loss:f}")
+
+# Plot learning results.
+plt.figure(figsize=(8, 4))
+plt.subplot(111)
+plt.plot(np.linspace(1, epochs_num, num=len(train_loss_track)), train_loss_track, label="Train loss")
+plt.plot(np.linspace(1, epochs_num, num=len(test_loss_track)), test_loss_track, label="Test loss")
+plt.title("title")
+plt.xlabel("Epochs")
+plt.ylabel("Value")
+plt.legend()
+plt.show()
+
+# Test network
+accuracy_sum = 0
+num = 0
+conf_mat = np.zeros((10, 10))
+for batch_num, (inputs, labels) in enumerate(train_loader):
+    outputs = net(inputs)
+    _, pred_labels = outputs.cpu().detach().max(1)
+    accuracy_sum += mtr.accuracy_score(labels.cpu(), pred_labels)
+    num += 1
+    for i in range(len(pred_labels)):
+        conf_mat[labels[i], pred_labels[i]] += 1
+
+accuracy = accuracy_sum / num
+print(f"\nTest set accuracy: {accuracy:.2f}")
+
+# Plot confusion matrix
+plt.figure(figsize=(6, 6))
+sns.heatmap(conf_mat, cmap="YlGnBu", annot=True, cbar=False, square=True)
+plt.xlabel("Predicted values")
+plt.ylabel("Actual values")
+plt.title(f"Test set accuracy: {accuracy:.2f}")
+plt.show()
---
+++ mnist/cnn_net_model.py
@@ -0,0 +1,48 @@
+import torch
+import torch.nn as nn
+
+
+class CnnNet(nn.Module):
+
+    def __init__(self):
+        super(CnnNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, padding=1)
+        self.conv2 = nn.Conv2d(32, 32, 3, padding=1)
+        self.conv3 = nn.Conv2d(32, 32, 5, stride=2, padding=2)
+
+        self.conv4 = nn.Conv2d(32, 64, 3, padding=1)
+        self.conv5 = nn.Conv2d(64, 64, 3, padding=1)
+        self.conv6 = nn.Conv2d(64, 64, 5, stride=2, padding=2)
+
+        self.fc1 = nn.Linear(3136, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+        self.dropout = nn.Dropout(p=0.4)  # TODO test 2d dropout
+        self.norm1 = nn.BatchNorm2d(32)
+        self.norm2 = nn.BatchNorm2d(64)
+        self.norm3 = nn.BatchNorm1d(128)
+
+        self.activation = nn.LeakyReLU()
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        x = self.activation(self.conv1(x))
+        x = self.activation(self.conv2(x))
+        x = self.activation(self.conv3(x))
+        x = self.norm1(x)
+        x = self.dropout(x)
+
+        x = self.activation(self.conv4(x))
+        x = self.activation(self.conv5(x))
+        x = self.activation(self.conv6(x))
+        x = self.norm2(x)
+        x = self.dropout(x)
+
+        x = torch.flatten(x, start_dim=1)
+        x = self.activation(self.fc1(x))
+        x = self.norm3(x)
+        x = self.dropout(x)
+        x = self.fc2(x)
+
+        x = self.softmax(x)
+        return x
